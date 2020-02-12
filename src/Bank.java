5606d7ffd47e9c6bde198647b2be210610d6b7ec
anishyadav063 <anishyadav063@gmail.com>
2020-02-12 15:08:02 +0530

    writing logic on deposite method

--- src/Bank.java
+++ src/Bank.java
@@ -1,25 +1,34 @@
 
 public interface Bank {
-	public void deposite(int amount);
+	public int deposite(int amount);
 	public void withdraw(int amount);
 
 }
 
 class Hdfs implements Bank{
-	double balance=10000;
+	int balance=10000;
 
 	@Override
-	public void deposite(int amount) {
-		  if(amount>balance) {
-			  throw new invalidAmountException("please enter valid amount");
+	public int deposite(int amount) {
+		 balance=balance+amount;
+		 return balance;
 		  }
 		
-	}
+	
 
 	@Override
 	public void withdraw(int amount) {
 		// TODO Auto-generated method stub
+		 if(amount>balance) {
+			  throw new invalidAmountException("please enter valid amount");
 		
 	}
-	
+	}
 }
+class HdfsBankTest{
+	public static void main(String[] args) {
+		Hdfs h=new Hdfs();
+		h.deposite(1000);
+	}
+}
+
