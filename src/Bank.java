cd4d528bf66ec78ec3ead0753871ed75cca34fdc
anishyadav063 <anishyadav063@gmail.com>
2020-02-12 14:41:13 +0530

    Added class Hdfs bank

--- src/Bank.java
+++ src/Bank.java
@@ -1,6 +1,23 @@
 
 public interface Bank {
-	public void deposite();
-	public void withdraw();
+	public void deposite(int amount);
+	public void withdraw(int amount);
 
+}
+
+class Hdfs implements Bank{
+	double balance=10000;
+
+	@Override
+	public void deposite(int amount) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void withdraw(int amount) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
